b1052ce37d45ca518221713f9e741f6c410b3cba
maciejc978 <czarneckimaciej78@gmail.com>
2026-02-21 16:07:33 +0100

    dodanie opcji dodawania obrazów

--- blog/models.py
+++ blog/models.py
@@ -7,13 +7,14 @@ class Post(models.Model):
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
+    image = models.ImageField(null=True, blank=True, upload_to='images/')
 
-    def publish(self):
-        self.publish_date = timezone.now()
-        self.save()
+def publish(self):
+    self.publish_date = timezone.now()
+    self.save()
 
-    def __str__(self):
-        return self.title
+def __str__(self):
+    return self.title
 
 
 
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,38 @@
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
-
 from .models import Post
+from django.http import HttpResponseRedirect
+from django.urls import reverse_lazy
+from .forms import ImgForm
+from django.views.generic import DetailView
+from django.views.generic import TemplateView
+
+
+class Image(TemplateView):
+    form = ImgForm
+    template_name = 'blog/image.html'
+
+class ImageDisplay(DetailView):
+    model = Post
+    template_name = 'blog/image_display.html'
+    context_object_name = 'image'
+
+def post(self, request, *args, **kwargs):
+    form=ImgForm(self.request.POST,request.FILES)
+    if form.is_valid():
+        obj = form.save()
+        return HttpResponseRedirect(reverse_lazy('image_display',kwargs={'pk':obj.id}))
+
+    context = self.get_context_data(form=form)
+    return self.render_to_response(context)
+
+def get(self, request, *args, **kwargs):
+    return self.get(request, *args, **kwargs)
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, 'blog/post_list.html',{"posts":posts})
+
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html',{"post":post})
